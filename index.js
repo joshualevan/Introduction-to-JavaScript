3688bcabe737a2281b7e454c04af21351db9ff61
Josh Levan <joshlevan@Joshs-iMac-2.local>
2021-03-16 16:15:02 -0400

    complete task 6

--- index.js
+++ index.js
@@ -237,8 +237,10 @@ Using the annoyingSong function below do the following:
       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong( /*add your code here*/ ) {
-    /*add your code here*/
+function annoyingSong(num) {
+    for (let i = num; i >= 0; i--) {
+        return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-1} bottles of soda on the wall`;
+    }
 }
 
 
